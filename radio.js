8413b24f0d07fcd0c8ec45210ed95b4d0ccb232a
evertongussilva <141072422+evertongussilva@users.noreply.github.com>
2023-10-02 16:51:02 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -2,53 +2,53 @@ const musicas = [
     {
         nome: "Let Go",
         artista: "Ark Patrol",
-        src: "Musicas/Ark Patrol - Let Go (ft. Veronika Redd) [Heroic]_Ts5ZiojkOe4.mp3",
+        src: "Músicas/Ark Patrol - Let Go (ft. Veronika Redd) [Heroic]_Ts5ZiojkOe4.mp3",
     },
     {
         nome: "Lovely",
         artista: "Billie Eilish",
-        src: "Musicas/Billie Eilish, Khalid - lovely (Official Music Video)_V1Pl8CzNzCw.mp3",
+        src: "Músicas/Billie Eilish, Khalid - lovely (Official Music Video)_V1Pl8CzNzCw.mp3",
     },
     {
         nome: "Idfc",
         artista: "Blackbear",
-        src: "Musicas/Blackbear - _Idfc_ (Acoustic) OFFICIAL VERSION_-N89TPQdX2o.mp3",
+        src: "Músicas/Blackbear - _Idfc_ (Acoustic) OFFICIAL VERSION_-N89TPQdX2o.mp3",
     },
     
     {
         nome: "I hate u, i love u ",
         artista: "Olivia o'brien",
-        src: "Musicas/gnash - i hate u, i love u ft. olivia o'brien (music video)_BiQIc7fG9pA.mp3",
+        src: "Músicas/gnash - i hate u, i love u ft. olivia o'brien (music video)_BiQIc7fG9pA.mp3",
     },
     {
         nome: "Beliver",
         artista: "Imagine Dragons",
-        src: "Musicas/Imagine Dragons - Believer (Official Music Video)_7wtfhZwyrcc.mp3",
+        src: "Músicas/Imagine Dragons - Believer (Official Music Video)_7wtfhZwyrcc.mp3",
     },
     {
         nome: "Bones",
         artista: "Imagine Dragons",
-        src: "Musicas/Imagine Dragons - Bones (Official Music Video)_TO-_3tck2tg.mp3",
+        src: "Músicas/Imagine Dragons - Bones (Official Music Video)_TO-_3tck2tg.mp3",
     },
     {
         nome: "Enemy",
         artista: "Imagine Dragons x J.I.D ",
-        src: "Musicas/Imagine Dragons x J.I.D - Enemy (from the series Arcane League of Legends)_D9G1VOjN_84.mp3",
+        src: "Músicas/Imagine Dragons x J.I.D - Enemy (from the series Arcane League of Legends)_D9G1VOjN_84.mp3",
     },
     {
         nome: "Good 4 u",
         artista: "Olivia Rodrigo",
-        src: "Musicas/Olivia Rodrigo - good 4 u (Official Video)_gNi_6U5Pm_o.mp3",
+        src: "Músicas/Olivia Rodrigo - good 4 u (Official Video)_gNi_6U5Pm_o.mp3",
     },
     {
         nome: "Set Fire to the Rain",
         artista: "Adele",
-        src: "Musicas/Set Fire to the Rain_uJdu4Lfy8aI.mp3",
+        src: "Músicas/Set Fire to the Rain_uJdu4Lfy8aI.mp3",
     },
     {
         nome: "Señorita",
         artista: "Shawn Mendes, Camila Cabello",
-        src: "Musicas/Shawn Mendes, Camila Cabello - Señorita_Pkh8UtuejGw.mp3",
+        src: "Músicas/Shawn Mendes, Camila Cabello - Señorita_Pkh8UtuejGw.mp3",
     },
 ];
 
